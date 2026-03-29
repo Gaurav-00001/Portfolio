d7b136802b5692bcc0d4ab2b4fc540b456defbc7
Gaurav <ranswalgaurav44@gmail.com>
2026-03-29 19:40:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,14 +47,14 @@ import ecomImg from './assets/image.png';
  <div className='Project-Container'>
   <div className='project-card'>
     <h2><a href="https://nivashub-theta.vercel.app/">• NivasHub</a></h2>
-    <img src="nivasImg" alt="PG"  />
+    <img src={nivasImg} alt="PG"  />
     <p>NivasHub | Student Housing Simplified. A streamlined PG discovery platform built with Supabase.</p>
     <a href="https://github.com/Gaurav-00001/PG-FINDER.git">REPO-Link</a>
   </div>
 
   <div className='project-card'>
     <h2><a href="https://gaurav-00001.github.io/e-commerce-website/">• Daily-Essentials</a></h2>
-    <img src="ecomImg" alt="E-commerce" />
+    <img src={ecomImg} alt="E-commerce" />
     <p>Daily Essentials Store is a responsive e-commerce storefront with a local storage cart.</p>
     <a href="https://github.com/Gaurav-00001/e-commerce-website.git">REPO-Link</a>
   </div>
