7fe0d11edbce20df5dcdb9a2772448f12a0f2464
Gaurav <ranswalgaurav44@gmail.com>
2026-03-30 23:32:39 +0530

    Integrate Form component and enhance project section
    Added a Form component and updated project details.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 import logo from './assets/CHASMISH CODER.png'
 import nivasImg from './assets/image1.png';
 import ecomImg from './assets/image.png';
+import Form from './components/Form.jsx';
+import Genaiimg from './assets/image2.png';
  const App = () => {
   return (
-    <nav>
+   
        <div className='container'>
       <h1> GAURAV'S PORTFOLIO</h1>
      <div className='about-me'> 
@@ -57,12 +59,43 @@ import ecomImg from './assets/image.png';
     <img src={ecomImg} alt="E-commerce" />
     <p>Daily Essentials Store is a responsive e-commerce storefront with a local storage cart.</p>
     <a href="https://github.com/Gaurav-00001/e-commerce-website.git">REPO-Link</a>
-  </div>
-</div>
+  
+ 
 </div>
 
-      </nav>
+  <div className='project-card'>
+    <h2><a href="https://gaurav-00001.github.io/GENAI/">• GEN-AI</a></h2>
+    <img src={Genaiimg} alt="GEN-AI" />
+    <p>A NxtWave-certified project demonstrating model integration and prompt engineering to create dynamic, contextually relevant conversational interfaces.</p>
+    <a href="https://github.com/Gaurav-00001/GENAI.git">REPO-Link</a>
+  
+ 
+</div>
+ </div>
+  <div className='Social-accounts' id='social'>
+    <a href="https://linkedin.com/in/your-profile" target="_blank" className="circle-link linkedin">
+    <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" height={25}/>
+  </a>
+    <a href="https://github.com/Gaurav-00001" target="_blank" className="circle-link github">
+    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" height={25}/>
+    </a>
+    <a href="https://www.instagram.com/gauravvv_ranswal" target="_blank" className="circle-link instagram">
+      <img src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="Instagram" height={25}/>
+      </a>
+    
+  </div>
+ <div className='contact-section'>
+  <Form />
+  
+  </div>
+</div>
+
+
+
+     
+      
       
   );
 };
+
 export default App;
